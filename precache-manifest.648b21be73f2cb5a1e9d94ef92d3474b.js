9ed6eddfd5c17c4855112aa9de3715b5262c113f
Sneha <gond0017@algonquinlive.com>
2020-10-07 18:50:46 -0400

    Updates

---
+++ precache-manifest.648b21be73f2cb5a1e9d94ef92d3474b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4518135fdd2fbad547b16f4a0af7703",
+    "url": "/mad9135-c1-react-router/index.html"
+  },
+  {
+    "revision": "86ffcc661dea6e5579e0",
+    "url": "/mad9135-c1-react-router/static/css/main.b646456d.chunk.css"
+  },
+  {
+    "revision": "2b0f4b76d2cc15949174",
+    "url": "/mad9135-c1-react-router/static/js/2.1b5d5e57.chunk.js"
+  },
+  {
+    "revision": "7e08df03892269e6e39da8a34916b217",
+    "url": "/mad9135-c1-react-router/static/js/2.1b5d5e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ffcc661dea6e5579e0",
+    "url": "/mad9135-c1-react-router/static/js/main.facedb39.chunk.js"
+  },
+  {
+    "revision": "24218efa06653e4f46cf",
+    "url": "/mad9135-c1-react-router/static/js/runtime-main.9d5132cf.js"
+  },
+  {
+    "revision": "0c1863738b78becd98eae4f3cb4c7693",
+    "url": "/mad9135-c1-react-router/static/media/profile_img.0c186373.png"
+  }
+]);
